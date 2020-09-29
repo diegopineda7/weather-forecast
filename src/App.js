94ae88b8b9472246cca8b403a97144c0f33eda36
Diego Pineda <pineda.diego798@gmail.com>
2020-09-28 22:20:13 -0500

    Set city name after api response

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Banner from './components/Banner';
 import CitiesWeather from './components/CitiesWeather';
 import DaysForecast from './components/DaysForecast';
@@ -7,19 +7,23 @@ import PlaceVisit from './components/PlaceVisit';
 import './styles/styles.css';
 
 function App() {
+  const [city, setCity] = useState('')
 
   useEffect(() => {
-    loadWeather()
+    loadWeather('bogota')
   }, [])
 
-  const loadWeather = () => {
-    const Http = new XMLHttpRequest();
-    const url = 'http://api.openweathermap.org/data/2.5/forecast?q=bogota&appid=d9808b61d1fc7e7235bb8e197bc5a6ac';
-    Http.open('GET', url);
-    Http.send();
+  const loadWeather = city => {
+    const Http = new XMLHttpRequest()
+    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=d9808b61d1fc7e7235bb8e197bc5a6ac`
+    Http.open('GET', url)
+    Http.send()
 
-    Http.onreadystatechange = (e) => {
-      console.log('RES', Http.responseText)
+    Http.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const result = JSON.parse(Http.responseText)
+        setCity(result.city.name)
+      }
     }
   }
 
@@ -27,8 +31,8 @@ function App() {
     <>
       <FixedCurrent />
       <div className='app'>
-        <Banner cityName='Bogotá' />
-        <div class='app__secondary'>
+        <Banner cityName={city} />
+        <div className='app__secondary'>
           <DaysForecast />
           <PlaceVisit />
           <CitiesWeather />

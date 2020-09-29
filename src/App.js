4da5a68f07cbccb0180d97e197e386dcaaa7af82
Diego Pineda <pineda.diego798@gmail.com>
2020-09-28 21:35:04 -0500

    Fixed current component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from './components/Banner';
 import CitiesWeather from './components/CitiesWeather';
 import DaysForecast from './components/DaysForecast';
+import FixedCurrent from './components/FixedCurrent';
 import PlaceVisit from './components/PlaceVisit';
 import './styles/styles.css';
 
 function App() {
   return (
     <div className="app">
+      <FixedCurrent />
       <Banner cityName='Bogotá' />
       <div class="app__secondary">
         <DaysForecast />
---
+++ src/components/FixedCurrent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function FixedCurrent() {
+  return (
+    <div className="current shadow">
+      <div className="current__icon">
+        icon
+      </div>
+      <div className="current__temp">
+        temp
+      </div>
+    </div>
+  )
+}

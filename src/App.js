8859652f24eb12e9f9f25ca2200750111cb159b1
Diego Pineda <pineda.diego798@gmail.com>
2020-09-29 05:56:56 -0500

    Get API data for current Bogotá weather

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Banner from './components/Banner';
 import CitiesWeather from './components/CitiesWeather';
 import DaysForecast from './components/DaysForecast';
 import FixedCurrent from './components/FixedCurrent';
 import PlaceVisit from './components/PlaceVisit';
-import { getCurrentWeather, getWeatherForecast } from './services';
+import { getCurrentWeather } from './services';
 import './styles/styles.css';
 
 function App() {
-  const [mainCity, setMainCity] = useState('')
-  const [mainCityWeather, setMainCityWeather] = useState({})
-  const [parisWeather, setParisWeather] = useState({})
-  const [cityWeather, setCityWeather] = useState({})
+  const [bogotaWeather, setBogotaWeather] = useState(undefined)
+  const [parisWeather, setParisWeather] = useState(undefined)
+  const [cityWeather, setCityWeather] = useState(undefined)
 
-  // useEffect(() => {
-  //   loadWeather()
-  // }, [])
+  useEffect(() => {
+    loadWeather()
+  }, [])
 
-  const loadWeather = () => {
-    const mainResult = getWeatherForecast('bogota')
-    const parisResult = getCurrentWeather('paris')
-    const cityResult = getCurrentWeather('madrid')
-    setMainCity(mainResult.city.name)
-    setMainCityWeather(mainResult)
-    setParisWeather(parisResult)
-    setCityWeather(cityResult)
+  const loadWeather = async () => {
+    getCurrentWeather('bogota', setBogotaWeather)
+    getCurrentWeather('paris', setParisWeather)
+    getCurrentWeather('madrid', setCityWeather)
   }
 
   return (
     <>
-      <FixedCurrent />
-      <div className='app'>
-        <Banner cityName={mainCity} />
-        <div className='app__secondary'>
-          <DaysForecast />
-          <PlaceVisit />
-          <CitiesWeather
-            paris={parisWeather}
-            city={cityWeather}
+      {
+        bogotaWeather &&
+        <>
+          <FixedCurrent
+            icon={bogotaWeather.weather[0].icon}
+            temp={bogotaWeather.main.temp}
+            weather={bogotaWeather.weather[0].main}
           />
-        </div>
-      </div>
+          <div className='app'>
+            <Banner cityName={bogotaWeather.name} />
+            <div className='app__secondary'>
+              <DaysForecast />
+              <PlaceVisit />
+              <CitiesWeather
+                paris={parisWeather}
+                city={cityWeather}
+              />
+            </div>
+          </div>
+        </>
+      }
     </>
   )
 }
--- src/services/index.js
+++ src/services/index.js
@@ -1,33 +1,34 @@
 const appId = 'd9808b61d1fc7e7235bb8e197bc5a6ac'
 
-export const getCurrentWeather = city => {
+export const getWeatherForecast = (city, next) => {
   var result = ''
   const Http = new XMLHttpRequest()
-  const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${appId}`
+  const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${appId}`
   Http.open('GET', url)
   Http.send()
 
   Http.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
       result = JSON.parse(Http.responseText)
+      next(result)
     }
   }
   return result
 }
 
-export const getWeatherForecast = city => {
-  var result = ''
+export const getCurrentWeather = (city, next) => {
   const Http = new XMLHttpRequest()
-  const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${appId}`
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${appId}`
   Http.open('GET', url)
   Http.send()
 
   Http.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      result = JSON.parse(Http.responseText)
+      const result = JSON.parse(Http.responseText)
+      console.log('RES', result)
+      next(result)
     }
   }
-  return result
 }
 
 export const iconUrl = iconName => {

e80cf9910d2613bdf12158ceff42baf3f6ec3090
Diego Pineda <pineda.diego798@gmail.com>
2020-09-28 22:05:10 -0500

    API call

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Banner from './components/Banner';
 import CitiesWeather from './components/CitiesWeather';
 import DaysForecast from './components/DaysForecast';
@@ -7,6 +7,22 @@ import PlaceVisit from './components/PlaceVisit';
 import './styles/styles.css';
 
 function App() {
+
+  useEffect(() => {
+    loadWeather()
+  }, [])
+
+  const loadWeather = () => {
+    const Http = new XMLHttpRequest();
+    const url = 'http://api.openweathermap.org/data/2.5/forecast?q=bogota&appid=d9808b61d1fc7e7235bb8e197bc5a6ac';
+    Http.open('GET', url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+      console.log('RES', Http.responseText)
+    }
+  }
+
   return (
     <>
       <FixedCurrent />

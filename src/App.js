0cc2ece03b039820bee8fdd0f50ee9f1da06dc0f
Diego Pineda <pineda.diego798@gmail.com>
2020-09-28 21:26:10 -0500

    Days forecast component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Banner from './components/Banner';
+import DaysForecast from './components/DaysForecast';
 import './styles/styles.css';
 
 function App() {
   return (
     <div className="app">
       <Banner cityName='Bogotá' />
+      <div class="app__secondary">
+        <DaysForecast />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/DaysForecast.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function DaysForecast() {
+  return (
+    <div className="days">
+      <div className="header">
+        <b>3 Days</b> Forecast
+          </div>
+      <div className="content--days">
+        <div className="day shadow">Day1</div>
+        <div className="day shadow">Day2</div>
+        <div className="day shadow">Day3</div>
+      </div>
+    </div>
+  )
+}

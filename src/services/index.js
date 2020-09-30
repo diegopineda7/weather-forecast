3bc1ab1325fa6fe8b707acd989dcadf622f164d7
Diego Pineda <pineda.diego798@gmail.com>
2020-09-29 21:28:39 -0500

    API request http -> https

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,6 @@
 const appId = process.env.REACT_APP_API_ID
 
 export const getWeatherForecast = (lat, lon, next) => {
-  var result = ''
   const Http = new XMLHttpRequest()
   const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=metric&appid=${appId}`
   Http.open('GET', url)
@@ -9,16 +8,15 @@ export const getWeatherForecast = (lat, lon, next) => {
 
   Http.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      result = JSON.parse(Http.responseText)
+      const result = JSON.parse(Http.responseText)
       next(result)
     }
   }
-  return result
 }
 
 export const getCurrentWeather = (city, next) => {
   const Http = new XMLHttpRequest()
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${appId}`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${appId}`
   Http.open('GET', url)
   Http.send()
 
@@ -31,5 +29,5 @@ export const getCurrentWeather = (city, next) => {
 }
 
 export const iconUrl = iconName => {
-  return `http://openweathermap.org/img/w/${iconName}.png`
+  return `https://openweathermap.org/img/w/${iconName}.png`
 }

fe1a8dda8c35b54d27fd913a4f0ae959c85f8ed8
Diego Pineda <pineda.diego798@gmail.com>
2020-09-29 21:15:51 -0500

    Updates

---
+++ precache-manifest.acd24b01bb516c0ce031aaf0e005d38e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec400d7f0dbc442aac5cd54c3131640",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "678f2e1be996f6039d3d",
+    "url": "/weather-forecast/static/css/main.d4a9b39b.chunk.css"
+  },
+  {
+    "revision": "699fbcb0ce9590e158ac",
+    "url": "/weather-forecast/static/js/2.187c6bad.chunk.js"
+  },
+  {
+    "revision": "093f9f43050dea01f335ef0d602ca915",
+    "url": "/weather-forecast/static/js/2.187c6bad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678f2e1be996f6039d3d",
+    "url": "/weather-forecast/static/js/main.eee64801.chunk.js"
+  },
+  {
+    "revision": "a96cd891956dfff3f9cf",
+    "url": "/weather-forecast/static/js/runtime-main.d4e096a0.js"
+  },
+  {
+    "revision": "b08e37b194a7fe8fe61fc12af6dc0fba",
+    "url": "/weather-forecast/static/media/person.b08e37b1.png"
+  }
+]);
